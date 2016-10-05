17e3921fe5cc68d9a19f9cb795bc1581a05ebdef
Devin Abbott <devinabbott@gmail.com>
2016-10-05 15:36:41 -0700

    Check if props have changed and re-render according

--- src/Enhancer.js
+++ src/Enhancer.js
@@ -20,17 +20,24 @@ export default (stylesCreator, selector, options) => {
 
     constructor(props, context) {
       super()
+      this.selectedProps = this.selectProps(props)
       this.state = this.mapContextToState(props, context)
-      this.selectedData = undefined
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
-      if (nextContext.theme !== this.context.theme) {
+      const nextSelectedProps = this.selectProps(nextProps)
+
+      if (
+        nextContext.theme !== this.context.theme ||
+        nextSelectedProps !== this.selectedProps
+      ) {
+        this.selectedProps = nextSelectedProps
         this.setState(this.mapContextToState(nextProps, nextContext))
       }
 
       // Update all styles on HMR regardless of theme change
       if (module.hot) {
+        this.selectedProps = nextSelectedProps
         this.setState(this.mapContextToState(nextProps, nextContext))
       }
     }
@@ -39,19 +46,27 @@ export default (stylesCreator, selector, options) => {
       const {theme} = context
 
       if (typeof selector === 'function') {
-
-        // TODO Handle getDefaultProps() for classic React?
-        const defaultProps = WrappedComponent.defaultProps
-
-        const selectedData = selector({...defaultProps, ...props})
-
-        if (!equal(this.selectedData, selectedData)) {
-          this.selectedData = selectedData
-        }
-
-        return {styles: createStyles(theme, this.selectedData)}
+        return {styles: createStyles(theme, this.selectedProps)}
       } else {
         return {styles: createStyles(theme)}
+      }
+    }
+
+    selectProps(props) {
+      if (typeof selector !== 'function') return undefined
+
+      // TODO Handle getDefaultProps() for classic React?
+      const defaultProps = WrappedComponent.defaultProps
+
+      const selectedProps = selector({...defaultProps, ...props})
+
+      // If equal, return the old selection
+      if (equal(this.selectedProps, selectedProps)) {
+        return this.selectedProps
+
+      // Else, return the new selection
+      } else {
+        return selectedProps
       }
     }
 
